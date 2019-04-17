6d2a1f66292eaff3fd203a97f9488dc12e9eabda
Frost giant Orange dartwing <forange-@at-b2.21-school.ru>
2019-04-17 22:09:10 +0300

    new Structure

--- get_next_line.h
+++ get_next_line.h
@@ -3,23 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kirill <kirill@student.42.fr>              +#+  +:+       +#+        */
+/*   By: forange- <forange-@student.fr.42>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 14:38:18 by forange-          #+#    #+#             */
-/*   Updated: 2019/04/16 15:20:53 by kirill           ###   ########.fr       */
+/*   Updated: 2019/04/17 21:34:48 by forange-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft/libft.h"
 # define BUFF_SIZE 32
 # define MAX_FD 4865 /* ulimit -n */
 # define EOL 0x0A
+
+typedef struct		s_node
+{
+	unsigned char	*content;
+	size_t			content_size;
+	unsigned char	*next;
+}					t_node;
 
 int	get_next_line(const int fd, char **line);
 
