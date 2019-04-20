af41496bd2bb456f21f96871ff5587ba9da639b7
Kirill <kirill@MacBook-Pro-Kirill.local>
2019-04-20 10:39:46 +0300

    len created

--- get_next_line.c
+++ get_next_line.c
@@ -3,41 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: forange- <forange-@student.fr.42>          +#+  +:+       +#+        */
+/*   By: kirill <kirill@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/16 15:13:19 by kirill            #+#    #+#             */
-/*   Updated: 2019/04/17 21:34:52 by forange-         ###   ########.fr       */
+/*   Updated: 2019/04/19 19:08:56 by kirill           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_list *ft_realloc(t_list *ptr, size_t size)
+t_node *ft_realloc(t_node *ptr, size_t size)
 {
-	t_list *temp;
+	t_node *temp;
 
-	temp = (t_list *)ft_memalloc(size * sizeof(t_list));
-	temp = ft_memcpy(temp, ptr, (ptr->next - ptr));
-	temp[0].next = temp + size - 1;
-	ft_memdel((void **)ptr);
+	if ((temp = (t_node *)ft_memalloc(size * sizeof(t_node))) != 0)
+	{
+		temp = ft_memcpy(temp, ptr, (ptr->arsize));
+		temp[0].arsize = size;
+		ft_memdel((void **)ptr);
+	}
 	return (temp);
 }
 
-/*
-**	В указатель next лежит:
-**	1) адрес последнего элемента массива
-**	2) NULL если по данному fd достигнут EOF
-*/
-int ft_readline(int fd, t_list **arr, char **line)
+int ft_readline(int fd, t_node **arr, char **line)
 {
 	if (!(*arr)[fd].content)
 	{
 		if (!((*arr)[fd].content = malloc(BUFF_SIZE)))
 			return (-1);
-		while (((*arr)[fd].content_size = read(fd, &(*arr)[fd].content, BUFF_SIZE)) > 0)
+		while (!(((*arr)[fd].content_size += read(fd, &(*arr)[fd].content, \
+				BUFF_SIZE)) % BUFF_SIZE))
 		{
-			*line = (char*)ft_memalloc((*arr)[fd].content_size);
-			ft_memccpy(*line, &(*arr)[fd].content, EOL, (*arr)[fd].content_size);
+			if (((*arr)[fd].ch = ft_memchr(&(*arr)[fd].content, EOL, (*arr)[fd].content_size)))
+			{
+				*line = (char*)malloc((unsigned char*)(*arr)[fd].ch - (unsigned char*)&(*arr)[fd].content);
+				ft_memccpy(*line, &(*arr)[fd].content, EOL, \
+						(*arr)[fd].content_size);
+			}/*
+			else
+			{
+				temp = (char*)malloc()
+			}
+*/
 		}
 	}
 	return (0);
@@ -45,19 +52,26 @@ int ft_readline(int fd, t_list **arr, char **line)
 
 int get_next_line(const int fd, char **line)
 {
-	static t_list *fd_ar;
+	static t_node *fd_ar;
 
 	if (!line || read(fd, NULL, 0) < 0)
 		return (-1);
 	if (!fd_ar)
 	{
-		if (!(fd_ar = (t_list *)ft_memalloc(sizeof(t_list) * MAX_FD)))
+		if (!(fd_ar = (t_node *)ft_memalloc(sizeof(t_node) * \
+			(fd >= MAX_FD ? fd + 1 : MAX_FD))))
 			return (-1);
-		fd_ar[0].next = fd_ar + MAX_FD - 1;
+		fd_ar[0].arsize = (fd >= MAX_FD ? fd + 1 : MAX_FD);
 	}
-	if (fd >= fd_ar[0].next - fd_ar)
-		fd_ar = ft_realloc(fd_ar, fd + 1);
-	if (*line)
-		ft_memdel((void **)line);
-	return (fd_ar ? ft_readline(fd, &fd_ar, line) : -1);
+	if (fd >= fd_ar[0].arsize)
+	{
+		if (!(fd_ar = ft_realloc(fd_ar, fd + 1)))
+			return (-1);
+	}
+	if (!ft_readline(fd, &fd_ar, line) && !(fd_ar[0].isany))
+	{
+		free(fd_ar);
+		fd_ar = NULL;
+	}
+	return (fd_ar ? (*line ? 1 : 0) : 0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: forange- <forange-@student.fr.42>          +#+  +:+       +#+        */
+/*   By: kirill <kirill@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 14:38:18 by forange-          #+#    #+#             */
-/*   Updated: 2019/04/17 21:34:48 by forange-         ###   ########.fr       */
+/*   Updated: 2019/04/19 19:11:30 by kirill           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft/libft.h"
-# define BUFF_SIZE 32
+# define BUFF_SIZE 8
 # define MAX_FD 4865 /* ulimit -n */
 # define EOL 0x0A
 
@@ -24,7 +24,9 @@ typedef struct		s_node
 {
 	unsigned char	*content;
 	size_t			content_size;
-	unsigned char	*next;
+	unsigned char	*ch;
+	void			*isany;
+	int				arsize;
 }					t_node;
 
 int	get_next_line(const int fd, char **line);
